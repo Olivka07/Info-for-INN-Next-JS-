a14341bafe98167ffa06161cc4690039596ad105
Olivka07 <uchenik.ikt@yandex.ru>
2024-05-18 19:56:05 +0300

    Some changes

--- src/components/ui/SearchButton/SearchButton.tsx
+++ src/components/ui/SearchButton/SearchButton.tsx
@@ -4,26 +4,25 @@ import React, { FC } from "react";
 import { Button, ThemeButton } from "../Button/Button";
 import classes from "./searchbutton.module.scss";
 import { classNames } from "@/utils/helpers/classNames";
-import { ThemeType } from "@/store/themeStore";
 import { useTheme } from "@/utils/hooks/useTheme";
 
 interface SearchButtonProps {
-    searchHandler: () => void;
+  searchHandler: () => void;
 }
 export const SearchButton: FC<SearchButtonProps> = (props) => {
-    const { theme } = useTheme();
-    const { searchHandler } = props;
-    return (
-        <Button
-            onClick={searchHandler}
-            className={classNames(classes.btn)}
-            theme={ThemeButton.CLEAR}
-        >
-            <img
-                className={classNames(classes.img)}
-                src={`/glass_${theme}.svg`}
-                alt='glass'
-            />
-        </Button>
-    );
+  const { theme } = useTheme();
+  const { searchHandler } = props;
+  return (
+    <Button
+      onClick={searchHandler}
+      className={classNames(classes.btn)}
+      theme={ThemeButton.CLEAR}
+    >
+      <img
+        className={classNames(classes.img)}
+        src={`/glass_${theme}.svg`}
+        alt='glass'
+      />
+    </Button>
+  );
 };
